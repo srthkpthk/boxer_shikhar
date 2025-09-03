48a6ec3226ebf39d8547d1340d87453c1d0aa175
Srthk Pthk <sarthak@pathak>
2025-09-03 19:05:21 +0530

    Fix package import reference in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:boxer/cubit/boxer_cubit.dart';
+import 'package:boxer_shikhar/cubit/boxer_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
